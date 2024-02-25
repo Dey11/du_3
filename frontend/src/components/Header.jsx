aa5d2875193d34a2b5269e3e3edb2d6321d4cd27
Dey11 <shreyandey11@gmail.com>
2024-02-25 15:39:10 +0530

    Implement auth for navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useContext, useEffect } from "react";
+import { UserContext } from "../contexts/UserContext";
 
 const Header = () => {
+  const { user, setUser } = useContext(UserContext);
+
+  const dataFetch = async () => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      axios
+        .get("http://localhost:3000/auth/validate", {
+          headers: {
+            authorization: token,
+          },
+        })
+        .then((res) => {
+          setUser(res.data);
+          console.log(user);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
+  useEffect(() => {
+    dataFetch();
+  }, []);
+
+  const username = user.username;
+
   return (
     <div className=" border-b-2 border-b-[#ffa1ab]">
       <nav className="py-3">
@@ -16,16 +46,34 @@ const Header = () => {
               About
             </li>
           </Link>
-          <Link to={"/login"}>
-            <li className="px-2 rounded-sm bg-[#ff6d7f] hover:bg-[#ffa1ab]">
-              Login
-            </li>
-          </Link>
-          <Link to={"/register"}>
-            <li className="px-2 rounded-sm bg-[#ff6d7f] hover:bg-[#ffa1ab]">
-              Register
-            </li>
-          </Link>
+          {!username && (
+            <Link to={"/login"}>
+              <li className="px-2 rounded-sm bg-[#ff6d7f] hover:bg-[#ffa1ab]">
+                Login
+              </li>
+            </Link>
+          )}
+          {username && (
+            <Link to={"/"}>
+              <li className="px-2 rounded-sm bg-[#ff6d7f] hover:bg-[#ffa1ab]">
+                Logout
+              </li>
+            </Link>
+          )}
+          {!username && (
+            <Link to={"/register"}>
+              <li className="px-2 rounded-sm bg-[#ff6d7f] hover:bg-[#ffa1ab]">
+                Register
+              </li>
+            </Link>
+          )}
+          {username && (
+            <Link to={"/"}>
+              <li className="px-2 rounded-sm bg-[#ff6d7f] hover:bg-[#ffa1ab]">
+                Dashboard
+              </li>
+            </Link>
+          )}
         </ul>
       </nav>
     </div>
