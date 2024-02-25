7fa0344a3c018d5e400b7dcaf55b2d4698ada352
Dey11 <shreyandey11@gmail.com>
2024-02-25 20:40:31 +0530

    Implement dashboard

--- frontend/src/components/ListItem.jsx
+++ frontend/src/components/ListItem.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-const ListItem = (props) => {
-  return <div>ListItem</div>;
+const ListItem = (item) => {
+  return (
+    <div className="container max-w-[1400px] mx-auto bg-[#ffa1ab] grid grid-cols-3 my-2">
+      <div className="col-span-1 ">
+        <img
+          src={item.item.image}
+          alt="placeholder"
+          className=" max-w-full max-h-fit"
+        />
+      </div>
+      <div className="col-span-2 ">
+        <h1 className="text-4xl">{item.item.state}</h1>
+        <h3 className="text-md">Latitude: {item.item.lat}</h3>
+        <h3 className="text-md">Longitude: {item.item.long}</h3>
+        <h4 className="text-sm">Time: {item.item.time}</h4>
+      </div>
+    </div>
+  );
 };
 
 export default ListItem;
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,39 +2,41 @@ import React from "react";
 import { useContext, useState, useEffect } from "react";
 import { UserContext } from "../contexts/UserContext";
 import axios from "axios";
+import ListItem from "../components/ListItem";
 
 const Dashboard = () => {
   const { user, setUser } = useContext(UserContext);
   const [data, setData] = useState(null);
 
-  useEffect(() => {
-    try {
-      const token = localStorage.getItem("token");
-      if (token) {
-        axios
-          .get(`http://localhost:3000/accidents/${user?.state}`, {
-            headers: {
-              authorization: token,
-            },
-          })
-          .then((res) => {
-            setData(res.data);
-            console.log(res.data);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      }
-    } catch (error) {
-      console.log(error);
+  const fetchData = async () => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      axios
+        .get(`http://localhost:3000/accidents/${user?.state}`, {
+          headers: {
+            authorization: token,
+          },
+        })
+        .then((res) => {
+          setData(res.data);
+          // console.log(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
+  };
+
+  useEffect(() => {
+    let timerId = setInterval(() => fetchData(), 2000);
+    return () => clearInterval(timerId);
   }, []);
 
   return (
     <div>
       <ul>
         {data?.map((item) => {
-          return <li key={item._id}>{item.severity}</li>;
+          return <ListItem key={item._id} item={item} />;
         })}
       </ul>
     </div>
